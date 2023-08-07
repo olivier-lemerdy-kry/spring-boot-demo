f04a2fb5343fa59ade471fb0375a4d9a47360362
Olivier Le Merdy <olivier.lemerdy@kry.se>
2023-08-07 15:33:03 +0200

    Change things

--- src/test/java/se/kry/springboot/demo/ApplicationTest.java
+++ src/test/java/se/kry/springboot/demo/ApplicationTest.java
@@ -5,9 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import se.kry.springboot.demo.infra.data.postgres.PostgresDefaults;
 
 @SpringBootTest
+@Testcontainers
 class ApplicationTest {
 
   @Container
